750526f723a03f793179cd14882888b734059f10
Yoqub06 <yoquballayev@gmail.com>
2025-10-28 02:37:45 +0500

    add block content

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
 from . import views
-
-
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('product/', views.product, name='product'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('compare/', views.compare, name='compare'),
+    path('catalog/', views.catalog, name='catalog'),
+    path('cart/', views.cart, name='cart'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
 
+
 def index(request):
-    ctx={
-
+    ctx = {
     }
+    return render(request, 'index.html', ctx)
 
-    return render(request, 'index.html',ctx)
 
+def product(request):
+    ctx = {
+    }
+    return render(request, 'product.html', ctx)
+
+
+def contacts(request):
+    ctx = {
+    }
+    return render(request, 'contacts.html', ctx)
+
+
+def compare(request):
+    ctx = {
+    }
+    return render(request, 'compare.html', ctx)
+
+
+def catalog(request):
+    ctx = {
+    }
+    return render(request, 'catalog.html', ctx)
+
+
+def cart(request):
+    ctx = {
+    }
+    return render(request, 'cart.html', ctx)
